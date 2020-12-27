dc7b103145b63ca152de611afc96cf29b94a37e6
goldbearman <sabirovfarit@google.com>
2020-12-27 19:04:09 +0300

    Заработал слайдер

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,42 +1,48 @@
-export const slider = document.querySelector('.swiper-container');
-export let mySwiper;
+// import Swiper, { Navigation, Pagination } from 'swiper';
+// import '../../node_modules/swiper/swiper-bundle.css';
+//
+// // configure Swiper to use modules
+// Swiper.use([Navigation, Pagination]);
+//
+// export const slider = document.querySelector('.swiper-container');
+// export let mySwiper;
+//
+// export function mobileSlider() {
+//     if (window.innerWidth < 768 && slider.dataset.mobile == 'false') {
+//         mySwiper = new Swiper('.image-slider', {
+//             // If we need pagination
+//             pagination: {
+//                 el: '.swiper-pagination',
+//                 clickable: true,
+//             },
+//             keyboard: {
+//                 //Включаем упрвление калвиатурой
+//                 enabled: true,
+//                 //Только когда слайдер в пределах вьюпорта
+//                 onlyInViewport: true,
+//             },
+//             mousewheel: {
+//                 sensitivity: 1,
+//             },
+//             grabCursor: true,
+//             slideToClickedSlide: true,
+//             slidesPerView: 'auto',
+//         });
+//         slider.dataset.mobile = 'true';
+//     }
+//     if (window.innerWidth > 768) {
+//         slider.dataset.mobile = 'false';
+//
+//         if (slider.classList.contains('swiper-container-initialized')) {
+//             mySwiper.destroy();
+//         }
+//     }
+// }
 
-export function mobileSlider() {
-    if (window.innerWidth < 768 && slider.dataset.mobile == 'false') {
-        mySwiper = new Swiper('.image-slider', {
-            // If we need pagination
-            pagination: {
-                el: '.swiper-pagination',
-                clickable: true,
-            },
-            keyboard: {
-                //Включаем упрвление калвиатурой
-                enabled: true,
-                //Только когда слайдер в пределах вьюпорта
-                onlyInViewport: true,
-            },
-            mousewheel: {
-                sensitivity: 1,
-            },
-            grabCursor: true,
-            slideToClickedSlide: true,
-            slidesPerView: 'auto',
-        });
-        slider.dataset.mobile = 'true';
-    }
-    if (window.innerWidth > 768) {
-        slider.dataset.mobile = 'false';
-
-        if (slider.classList.contains('swiper-container-initialized')) {
-            mySwiper.destroy();
-        }
-    }
-}
-
-mobileSlider();
-window.addEventListener('resize', () => {
-    mobileSlider();
-});
+// mobileSlider();
+// window.addEventListener('resize', () => {
+//     mobileSlider();
+// });
 
 // var makeElement = function (tagName, className, text) {
 //     var element = document.createElement(tagName);
